e76f31b1fdf2a43072158759b740659c853fa879
刘华军 <lhjok@live.cn>
2023-05-03 01:09:06 +0800

    Update content

--- src/views/pages/content/mod.rs
+++ src/views/pages/content/mod.rs
@@ -4,27 +4,17 @@ use yew::prelude::*;
 pub use self::index::Index;
 pub use self::config::Config;
 
-// Content容器组件
-pub struct Content;
-
-#[derive(Properties, PartialEq, Clone)]
+#[derive(Properties, PartialEq)]
 pub struct Props {
     pub children: Children,
 }
 
-impl Component for Content {
-    type Message = ();
-    type Properties = Props;
-
-    fn create(_: &Context<Self>) -> Self {
-        Content {}
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <div id="content">
-                { for ctx.props().children.iter() }
-            </div>
-        }
+// Content容器组件
+#[function_component(Content)]
+pub fn content(props: &Props) -> Html {
+    html! {
+        <div id="content">
+            { for props.children.iter() }
+        </div>
     }
 }
